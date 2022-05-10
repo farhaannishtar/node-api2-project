945860476009575f859506a24ee4812646ae6dca
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-05-10 15:43:12 -0400

    Sanity Check

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log("Work harrrd so you can enjoy your life with your new friends :)))");
+})
